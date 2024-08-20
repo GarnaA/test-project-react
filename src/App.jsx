5449dcc330d4b795cf2359ffee2676640c8def76
Alina Zakharchenko <alina.zakharchenko03@gmail.com>
2024-08-20 23:43:02 +0200

    add information about user

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
 import './App.css'
+import user from "./user.json";
+
+
 
 function App() {
-  return <main className='app'>Hello World</main>
+  const mailMe = "mailto:" + user.email;
+  return <main className='app'>
+    <h1>About me</h1>
+    <div>Name: {user.name} </div>
+    <div>
+      Skills
+      <div>css: {user.skills.css} </div>
+    </div>
+    <ul>   
+      {user.hobbies.map((hobby) => (
+        <li key={hobby}>{hobby}</li>
+      ))}
+      {/* Another way:
+      {user.hobbies.map((hobby) => (
+      return <li key={hobby}>{hobby}</li>;
+      ))}
+      */}
+    </ul>
+
+    <a href={{mailMe}}>Email me</a>
+  </main>
 }
 
 export default App
